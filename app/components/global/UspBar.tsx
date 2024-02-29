d8060cdb36b2968ecbdf3b45f626be95f83d537e
Emma Dolan <info@untetheredcollective.com>
2024-02-29 15:10:10 +0000

    Lesson 5 replace useMatches with useRooterLoaderData

--- app/components/global/UspBar.tsx
+++ app/components/global/UspBar.tsx
@@ -1,21 +1,18 @@
-import {useMatches} from '@remix-run/react';
+import {useRootLoaderData} from '~/root';
 
 export default function UspBar() {
-
-    const [root] = useMatches();
-    const uspBarData = root?.data?.layout?.uspBar;
-
-    console.log(uspBarData, 'uspBarData')
+    const {layout} = useRootLoaderData();
+    const uspBarData = layout?.uspBar;
 
     return (
         <div className="w-full bg-black text-white top-0 lg:flex uspbar fixed left-0 z-50">
-            {uspBarData && (
-                <ul className="flex w-full">
-                    <li className="w-1/3 flex justify-center p-2">{uspBarData.usp1}</li>
-                    <li className="w-1/3 flex justify-center p-2">{uspBarData.usp2}</li>
-                    <li className="w-1/3 flex justify-center p-2">{uspBarData.usp3}</li>
-                </ul>
-            )}
+                {uspBarData && (
+                    <ul className="flex w-full">
+                        <li className="w-1/3 flex justify-center p-2">{uspBarData.usp1}</li>
+                        <li className="w-1/3 flex justify-center p-2">{uspBarData.usp2}</li>
+                        <li className="w-1/3 flex justify-center p-2">{uspBarData.usp3}</li>
+                    </ul>
+                )}
         </div>
     )
 }
